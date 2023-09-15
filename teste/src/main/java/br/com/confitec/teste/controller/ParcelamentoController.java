98d77fd76b6eb31b727025e88ff09b2b4f552cfc
Marcelo Miller <marcelomillergo@gmail.com>
2023-09-15 15:06:22 -0300

    remove imports

--- teste/src/main/java/br/com/confitec/teste/controller/ParcelamentoController.java
+++ teste/src/main/java/br/com/confitec/teste/controller/ParcelamentoController.java
@@ -2,15 +2,12 @@ package br.com.confitec.teste.controller;
 
 import br.com.confitec.teste.model.ApolicePagamento;
 import br.com.confitec.teste.model.DadosParcelamento;
-import br.com.confitec.teste.model.ListaCobertura;
 import br.com.confitec.teste.service.ParcelamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +19,7 @@ public class ParcelamentoController {
     @PostMapping(value = "/confitec/teste/parcelamento", consumes = "application/json")
     public ResponseEntity<DadosParcelamento> parcelamento(@RequestBody ApolicePagamento request) {
         DadosParcelamento response = parcelamentoService.parcelamento(request);
-        System.out.println("Teste");
+
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
